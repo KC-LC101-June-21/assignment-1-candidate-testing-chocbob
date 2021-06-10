dd50eb69c75790bef0a8f1eec674eca978eab864
calebroman <85118611+chocbob@users.noreply.github.com>
2021-06-10 00:08:35 +0000

    Removed the code inside of the askQuestion function and replaced with a loop. Other minor cleanup to fulfill part 1 and part 2 of assigment.

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,35 +3,54 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName = input.question("Please enter your first and last name: ");
+let candidateName = ""
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? "
 let correctAnswer = "Sally Ride"
 let candidateAnswer = ""
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "]
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"]
+let candidateAnswers = []
 
 
-function askForName(candidateName) {
+function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+candidateName = input.question("Please enter your first and last name: ");
+console.log("Hello, " + candidateName)
 }
 
 function askQuestion() {
-  console.log(question)
-let candidateAnswer = input.question(": ")
+  for (i=0;i<questions.length;i++){
+    candidateAnswer = input.question(questions[i]);
+   candidateAnswers.push(candidateAnswer)
+  } 
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-}
+} 
+
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer === correctAnswer){
-  console.log("Correct!")
-} else {
-  console.log("Incorrect.")
-}
+console.log(`
+1) Who was the first American woman in space?
+Your Answer: ${candidateAnswers[0]}
+Correct Answer: Sally Ride
+
+2) True or false: 5000 meters = 5 kilometers.
+Your Answer: ${candidateAnswers[1]}
+Correct Answer: true
+
+3) (5 + 3)/2 * 10 = ?
+Your Answer: ${candidateAnswers[2]}
+Correct Answer: 40
+
+4) Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2?
+Your Answer: ${candidateAnswers[3]}
+Correct Answer: Trajectory
+
+5) What is the minimum crew size for the ISS?
+Your Answer: ${candidateAnswers[4]}
+Correct Answer: 3`)
 
   let grade;
   
@@ -40,9 +59,8 @@ if (candidateAnswer === correctAnswer){
 }
 
 function runProgram() {
-  askForName(candidateName);
+  askForName();
   // TODO 1.1c: Ask for candidate's name //
-  console.log("Hello, " + candidateName);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
