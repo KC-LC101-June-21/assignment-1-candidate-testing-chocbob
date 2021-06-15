47c403f61465d9563fde016905ede6b8fdbba594
calebroman <85118611+chocbob@users.noreply.github.com>
2021-06-15 15:27:34 +0000

    Fulfilled requirements for Assignment 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,7 +11,9 @@ let candidateAnswer = ""
 let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "]
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"]
 let candidateAnswers = []
-
+let candidateCorrectAnswers = []
+let candidateIncorrectAnswers = []
+let score = 0
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -23,47 +25,58 @@ function askQuestion() {
   for (i=0;i<questions.length;i++){
     candidateAnswer = input.question(questions[i]);
    candidateAnswers.push(candidateAnswer)
-  } 
+   if (candidateAnswer.toLowerCase() === correctAnswers[i].toLowerCase()) {
+    console.log("Correct"), score += 1;
+  } else {
+    console.log("Incorrect.")
+  // console.log(candidateAnswer.toLowerCase() + correctAnswers[i].toLowerCase())
+  }
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-} 
+}} 
 
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-console.log(`
-1) Who was the first American woman in space?
-Your Answer: ${candidateAnswers[0]}
-Correct Answer: Sally Ride
-
-2) True or false: 5000 meters = 5 kilometers.
-Your Answer: ${candidateAnswers[1]}
-Correct Answer: true
-
-3) (5 + 3)/2 * 10 = ?
-Your Answer: ${candidateAnswers[2]}
-Correct Answer: 40
-
-4) Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2?
-Your Answer: ${candidateAnswers[3]}
-Correct Answer: Trajectory
-
-5) What is the minimum crew size for the ISS?
-Your Answer: ${candidateAnswers[4]}
-Correct Answer: 3`)
-
-  let grade;
-  
-
-  return grade;
+passOrFail = score
+if (passOrFail >= 4){
+  passOrFail = "PASSED"
+} else {
+  passOrFail = "FAILED"
 }
+  grade = (score/5)*100
+  return grade;
+  }
+
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
   askQuestion();
   gradeQuiz(this.candidateAnswers);
-}
+  console.log(`
+1) Who was the first American woman in space?
+Your Answer: ${candidateAnswers[0]}
+Correct Answer: ${correctAnswers[0]}
+
+2) True or false: 5000 meters = 5 kilometers.
+Your Answer: ${candidateAnswers[1]}
+Correct Answer: ${correctAnswers[1]}
+
+3) (5 + 3)/2 * 10 = ?
+Your Answer: ${candidateAnswers[2]}
+Correct Answer: ${correctAnswers[2]}
+
+4) Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2?
+Your Answer: ${candidateAnswers[3]}
+Correct Answer: ${correctAnswers[3]}
+
+5) What is the minimum crew size for the ISS?
+Your Answer: ${candidateAnswers[4]}
+Correct Answer: ${correctAnswers[4]}
+
+>>> Overall Grade: ${grade} <<<
+>>> Status: ${passOrFail} <<<`)}
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
